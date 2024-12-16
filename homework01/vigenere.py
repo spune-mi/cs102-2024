f33556739575761ded453cedca71441fc599dce0
spune-mi <113019268+spune-mi@users.noreply.github.com>
2024-12-16 19:35:26 +0300

    Реализован шифр Виженера

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,6 +1,7 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -8,14 +9,38 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    ciphertext = []
+    for i in range(len(plaintext)):
+        if i > len(keyword) - 1:
+            if ord(keyword[i % len(keyword)]) >= 65 and ord(keyword[i % len(keyword)]) <= 90:
+                shift = ord(keyword[i % len(keyword)]) - ord("A")
+            else:
+                shift = ord(keyword[i % len(keyword)]) - ord("a")
+        else:
+            if ord(keyword[i]) >= 65 and ord(keyword[i]) <= 90:
+                shift = ord(keyword[i]) - ord("A")
+            else:
+                shift = ord(keyword[i]) - ord("a")
+
+        if ord(plaintext[i]) >= 65 and ord(plaintext[i]) <= 90:
+            b = chr(ord(plaintext[i]) + shift)
+            if ord(b) > 90:
+                b = chr(ord(b) - 26)
+            ciphertext.append(b)
+        elif ord(plaintext[i]) >= 97 and ord(plaintext[i]) <= 122:
+            b = chr(ord(plaintext[i]) + shift)
+            if ord(b) > 122:
+                b = chr(ord(b) - 26)
+            ciphertext.append(b)
+        else:
+            ciphertext.append(plaintext[i])
+    return "".join([str(i) for i in ciphertext])
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -23,6 +48,29 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
+    plaintext = []
+    for i in range(len(ciphertext)):
+        if i > len(keyword) - 1:
+            if ord(keyword[i % len(keyword)]) >= 65 and ord(keyword[i % len(keyword)]) <= 90:
+                shift = ord(keyword[i % len(keyword)]) - ord("A")
+            else:
+                shift = ord(keyword[i % len(keyword)]) - ord("a")
+        else:
+            if ord(keyword[i]) >= 65 and ord(keyword[i]) <= 90:
+                shift = ord(keyword[i]) - ord("A")
+            else:
+                shift = ord(keyword[i]) - ord("a")
+
+        if ord(ciphertext[i]) >= 65 and ord(ciphertext[i]) <= 90:
+            b = chr(ord(ciphertext[i]) - shift)
+            if ord(b) < 65:
+                b = chr(ord(b) + 26)
+            plaintext.append(b)
+        elif ord(ciphertext[i]) >= 97 and ord(ciphertext[i]) <= 122:
+            b = chr(ord(ciphertext[i]) - shift)
+            if ord(b) < 97:
+                b = chr(ord(b) + 26)
+            plaintext.append(b)
+        else:
+            plaintext.append(ciphertext[i])
+    return "".join([str(i) for i in plaintext])
